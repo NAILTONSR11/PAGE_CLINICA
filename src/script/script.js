026339f196e306e1d52cfca954ac3b2e15ffb84b
Nailton Rodrigues <nailtonsr01@mail.com>
2025-08-16 23:42:55 -0300

    Criação das sections de Estrutura, Localização e Redes Sociais

--- src/script/script.js
+++ src/script/script.js
@@ -46,3 +46,52 @@ botaoClose.addEventListener("click", () => {
   }, 400);
 });
 
+
+/* SLIDE ESTRUTURA */
+const imagenEstru = [
+  '/src/assets/imgs/recepcao.png',
+  '/src/assets/imgs/Area_Kids.png',
+  '/src/assets/imgs/RaioX.png',
+  '/src/assets/imgs/consultório.png',
+  '/src/assets/imgs/corredor.png'
+];
+
+let indiceEstru = 0;
+const Estru = document.querySelector('.estrutura-image');
+
+// Função para trocar a imagem
+function trocarImagem1() {
+  Estru.style.backgroundImage = `url('${imagenEstru[indiceEstru]}')`;
+  indiceEstru = (indiceEstru + 1) % imagenEstru.length; // volta ao início
+}
+
+// Troca a cada 5 segundos
+trocarImagem1(); // define a primeira
+setInterval(trocarImagem1, 4000);
+
+
+const minImg1 = document.querySelector('.minImg1');
+const minImg2 = document.querySelector('.minImg2');
+const minImg3 = document.querySelector('.minImg3');
+const minImg4 = document.querySelector('.minImg4');
+const minImg5 = document.querySelector('.minImg5');
+
+minImg1.addEventListener('click', () =>{
+  Estru.style.backgroundImage = `url('/src/assets/imgs/recepcao.png')`
+})
+minImg2.addEventListener('click', () => {
+  Estru.style.backgroundImage = `url(${imagenEstru[1]})`;
+});
+
+minImg3.addEventListener('click', () => {
+  Estru.style.backgroundImage = `url(${imagenEstru[2]})`;
+});
+
+minImg4.addEventListener('click', () => {
+  Estru.style.backgroundImage = `url(${imagenEstru[3]})`;
+});
+
+minImg5.addEventListener('click', () => {
+  Estru.style.backgroundImage = `url(${imagenEstru[4]})`;
+});
+
